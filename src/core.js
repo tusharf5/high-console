ecd176f528497a433d79f981785f84dd48d544e1
Tushar Sharma <ts17995@gmail.com>
2018-06-20 00:17:56 +0530

    Added Node Support

--- src/core.js
+++ src/core.js
@@ -17,6 +17,16 @@ function HighConsole(config) {
   configObject = Object.assign({}, configObject, config);
 }
 
+function isWindow() {
+  if (
+    typeof document !== "undefined" &&
+    typeof document.createElement !== "undefined"
+  ) {
+    return true;
+  }
+  return false;
+}
+
 const types = {
   debug: "debug",
   warn: "warn",
@@ -24,7 +34,7 @@ const types = {
   success: "success"
 };
 
-const theme = {
+const browserTheme = {
   dayDev: {
     debug: {
       color: "#2e2e2e",
@@ -57,6 +67,31 @@ const theme = {
   }
 };
 
+const nodeTheme = {
+  dayDev: {
+    debug: {
+      color: "\033[30m",
+      backgroundColor: "#fff",
+      textTransform: () => (configObject.upperCase ? "uppercase" : "none")
+    },
+    warn: {
+      color: "\x1b[33m",
+      backgroundColor: "#fff",
+      textTransform: () => (configObject.upperCase ? "uppercase" : "none")
+    },
+    error: {
+      color: "\033[31m",
+      backgroundColor: "#fff",
+      textTransform: () => (configObject.upperCase ? "uppercase" : "none")
+    },
+    success: {
+      color: "\033[32m",
+      backgroundColor: "#fff",
+      textTransform: () => (configObject.upperCase ? "uppercase" : "none")
+    }
+  }
+};
+
 const symbols = {
   dayDev: {
     debug: "●",
@@ -81,12 +116,22 @@ function makeStyleOutOfObject(obj) {
   }, "");
 }
 
-function printMessage(type, message, data) {
+function printBrowserMessage(type, message, data) {
   configObject.enabled &&
     console.log(
       `%c%s %o\n`,
-      makeStyleOutOfObject(theme[configObject.mode][type]),
+      makeStyleOutOfObject(browserTheme[configObject.mode][type]),
       `${symbols[configObject.mode][type]} ${message}`,
+      data
+    );
+}
+
+function printNodeMessage(type, message, data) {
+  configObject.enabled &&
+    console.log(
+      `${nodeTheme[configObject.mode][type].color} %s`,
+      `${symbols[configObject.mode][type]} ${message}`,
+      "\033[0m",
       data
     );
 }
@@ -111,20 +156,36 @@ function getMessageAndData(args) {
 
 function debug() {
   let { message, data } = getMessageAndData(arguments);
-  printMessage(types.debug, message, data);
+  if (isWindow()) {
+    printBrowserMessage(types.debug, message, data);
+  } else {
+    printNodeMessage(types.debug, message, data);
+  }
 }
 
 function warn() {
   let { message, data } = getMessageAndData(arguments);
-  printMessage(types.warn, message, data);
+  if (isWindow()) {
+    printBrowserMessage(types.warn, message, data);
+  } else {
+    printNodeMessage(types.warn, message, data);
+  }
 }
 
 function success() {
   let { message, data } = getMessageAndData(arguments);
-  printMessage(types.success, message, data);
+  if (isWindow()) {
+    printBrowserMessage(types.success, message, data);
+  } else {
+    printNodeMessage(types.success, message, data);
+  }
 }
 
 function error() {
   let { message, data } = getMessageAndData(arguments);
-  printMessage(types.error, message, data);
+  if (isWindow()) {
+    printBrowserMessage(types.error, message, data);
+  } else {
+    printNodeMessage(types.error, message, data);
+  }
 }
